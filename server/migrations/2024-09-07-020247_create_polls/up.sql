2d8147ff88a8dfd96d3da18c266aa5dbbf113a8f
Steven Vergenz <1882376+stevenvergenz@users.noreply.github.com>
2024-09-06 20:24:29 -0700

    Start on db setup

---
+++ server/migrations/2024-09-07-020247_create_polls/up.sql
@@ -0,0 +1,26 @@
+-- Your SQL goes here
+ALTER DATABASE postgres WITH ENCODING 'UTF8';
+
+CREATE TABLE IF NOT EXISTS Users (
+    id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    display_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Polls (
+    id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    title VARCHAR(300) NOT NULL,
+    winner_count INTEGER NOT NULL DEFAULT 1,
+    write_ins_allowed BOOLEAN NOT NULL DEFAULT FALSE,
+    close_after_time DATETIME,
+    close_after_votes INTEGER,
+    owner_id UUID NOT NULL,
+    FOREIGN KEY owner_id REFERENCES Users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS PollOptions (
+    poll_id UUID NOT NULL,
+    id INTEGER NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    PRIMARY KEY (poll_id, id),
+    FOREIGN KEY poll_id REFERENCES Polls (id) ON DELETE CASCADE
+);
---
+++ server/src/web/db.rs
@@ -0,0 +1,5 @@
+use std::collections::HashMap;
+
+use diesel::PgConnection;
+
+use super::super::voting::{Poll, PollOption, User, Id, RelativeId};
